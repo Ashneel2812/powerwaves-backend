26c004cb5a0e08845d020ab7d6cf2bf6bd91cda0
Ashneel <iashneel@gmail.com>
2025-06-20 18:30:50 +0530

    left one file

---
+++ models/Subscribers.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const subscriberSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  phone: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    trim: true,
+  },
+  location: {
+    type: String,
+    trim: true
+  },
+  submittedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Subscriber", subscriberSchema);
